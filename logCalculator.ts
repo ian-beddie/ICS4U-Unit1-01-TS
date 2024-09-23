ab86a298058185e2d948f83af957933762c8f299
Ian Beddie <ian.beddie@mths.ca>
2024-09-23 03:19:13 +0000

    added log calculator program

---
+++ logCalculator.ts
@@ -0,0 +1,23 @@
+/**
+ * The program shows how to get input and deal with numbers.
+ *
+ * By: Ian Beddie
+ * Version: 1.0
+ * Since:   2024-09-22
+ *
+ */
+
+
+import { createPrompt } from 'bun-promptx'
+
+
+const logLengthString = createPrompt('Select the length of your logs: 0.25m, 0.5m, 1m: ')
+
+let logLength = parseFloat(logLengthString.value)
+
+let logNumber = 1100 / (20 * logLength)
+
+console.log(`The truck can carry ${logNumber} logs that are ${logLength}m long.`)
+
+
+console.log('\nDone.')
