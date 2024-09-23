dac33ffed863a53ea6009297b3b451c8151f5614
Ian Beddie <ian.beddie@mths.ca>
2024-09-23 13:29:31 +0000

    added proper style

--- logCalculator.ts
+++ logCalculator.ts
@@ -7,17 +7,16 @@
  *
  */
 
-
 import { createPrompt } from 'bun-promptx'
 
+const logLengthString = createPrompt(
+  'Select the length of your logs: 0.25m, 0.5m, 1m: ')
 
-const logLengthString = createPrompt('Select the length of your logs: 0.25m, 0.5m, 1m: ')
+const logLength = parseFloat(logLengthString.value)
 
-let logLength = parseFloat(logLengthString.value)
+const logNumber = 1100 / (20 * logLength)
 
-let logNumber = 1100 / (20 * logLength)
-
-console.log(`The truck can carry ${logNumber} logs that are ${logLength}m long.`)
-
+console.log(
+  `The truck can carry ${logNumber} logs that are ${logLength}m long.`)
 
 console.log('\nDone.')
